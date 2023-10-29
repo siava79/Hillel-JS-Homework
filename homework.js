61e8f4591df363b5c93888789c5bf4c129ea203c
siava79 <blacksea448@gmail.com>
2023-10-29 20:45:06 +0300

    my first homework JS 2

--- homework.js
+++ homework.js
@@ -1,27 +1,65 @@
 
 //1) Create a variable with the name of our planet. 
 
+var OurPlanetName = "Earth";
+
 //2) Create a variable to store the name of the current site visitor.
+
+var currentVisitorName = "Viacheslav";
 
 //3) Create "var" variable named myAge and set its value equal to 30
 
+var myAge = 30;
+
 //4) Create "let" variable named myName and set its value equal to any name
+
+let myName = "Viacheslav"
 
 //5) Create "const" variable named myBool and set its value equal to false
 
+const myBool = false;
+
 //6) Display the sum of 15 + 30, using two variables: x and y.
 
+var x = 15;
+var y = 30;
+var sum = x + y;
+console.log("The sum of 15 + 30 = " + sum);
+
 //7) Create variable named myUndefined and set its value equal to undefined
+
+var myUndefined = undefined;
 
 //8) Create variable named dividedNumber and set its value equal myAge divided by 2
 // * NOTE: you should use myAge variable here
 
+
+var dividedNumber = myAge / 2;
+console.log(dividedNumber);
+
 //9) Create variable named multipleNumber and set its value equal myAge multiply by 2
+
+
+var multipleNumber = myAge * 2;
+console.log(multipleNumber);
 
 //10) Create variable named mySentence and set its value to be myName, then goes space and word is, then space and myAge
 //NOTE: Result should be something like "Michael is 30"
+
+var mySentence = myName;
+console.log(myName + " is " + myAge + " years.");
  
 //11) Create variable named myUndefined2 and set its value equal to undefined without "undefined" word usage
 
+var myUndefined2;
+console.log(myUndefined2);
+
 //12) Declare two variables: admin and name. Assign the value "Bill Gates" to the variable name.Copy the value from the variable name to admin.
 //Print the value of the admin variable.
+
+var Admin;
+var Name;
+Name = "Bill Gates";
+Admin = Name;
+console.log(Admin);
+
