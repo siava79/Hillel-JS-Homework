e977cd164694e436678380560455b28901262185
siava79 <blacksea448@gmail.com>
2023-10-29 20:44:04 +0300

    my first homework JS

---
+++ homework.js
@@ -0,0 +1,27 @@
+
+//1) Create a variable with the name of our planet. 
+
+//2) Create a variable to store the name of the current site visitor.
+
+//3) Create "var" variable named myAge and set its value equal to 30
+
+//4) Create "let" variable named myName and set its value equal to any name
+
+//5) Create "const" variable named myBool and set its value equal to false
+
+//6) Display the sum of 15 + 30, using two variables: x and y.
+
+//7) Create variable named myUndefined and set its value equal to undefined
+
+//8) Create variable named dividedNumber and set its value equal myAge divided by 2
+// * NOTE: you should use myAge variable here
+
+//9) Create variable named multipleNumber and set its value equal myAge multiply by 2
+
+//10) Create variable named mySentence and set its value to be myName, then goes space and word is, then space and myAge
+//NOTE: Result should be something like "Michael is 30"
+ 
+//11) Create variable named myUndefined2 and set its value equal to undefined without "undefined" word usage
+
+//12) Declare two variables: admin and name. Assign the value "Bill Gates" to the variable name.Copy the value from the variable name to admin.
+//Print the value of the admin variable.
